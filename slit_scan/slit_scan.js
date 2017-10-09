0af7efe0918a825df427f37203882f830a6f14e7
Francisco <franciscouzo@gmail.com>
2017-10-09 19:28:21 -0300

    Reorder and add new projects to index

---
+++ slit_scan/slit_scan.js
@@ -0,0 +1,264 @@
+function buffer_equal(buf1, buf2) {
+    if (buf1.byteLength != buf2.byteLength) return false;
+    var dv1 = new Int8Array(buf1);
+    var dv2 = new Int8Array(buf2);
+    for (var i = 0; i != buf1.byteLength; i++) {
+        if (dv1[i] != dv2[i]) return false;
+    }
+    return true;
+}
+
+function modify_line(line, x1, y1, x2, y2) {
+  var line_element = function(line, x, y, length, angle) {
+    var transform_properties = ['transform', 'MozTransform', 'WebkitTransform', 'msTransform', 'oTransform'];
+
+    line.style.border = '1px solid red';
+    line.style.width = length + 'px';
+    line.style.height = '0px';
+
+    for (var i = 0; i < transform_properties.length; i++) {
+      line.style[transform_properties[i]] = 'rotate(' + angle + 'rad)';
+    }
+
+    line.style.position = 'absolute';
+    line.style.top = y + 'px';
+    line.style.left = x + 'px';
+    line.style.pointerEvents = 'none';
+
+    return line;
+  };
+
+  var a = x1 - x2,
+      b = y1 - y2,
+      c = Math.hypot(a, b);
+
+  var sx = (x1 + x2) / 2,
+      sy = (y1 + y2) / 2;
+
+  var x = sx - c / 2,
+      y = sy;
+
+  var alpha = Math.PI - Math.atan2(-b, a);
+
+  return line_element(line, x, y, c, alpha);
+}
+
+function hide_gui_element(gui, property, hide) {
+  for (var i = 0; i < gui.__controllers.length; i++) {
+    var controller = gui.__controllers[i];
+    if (controller.property === property) {
+      controller.domElement.parentElement.parentElement.hidden = hide;
+      return;
+    }
+  }
+}
+
+function hide_gui_folder(gui, folder_name, hide) {
+  var folder = gui.__folders[folder_name];
+  folder.domElement.parentElement.hidden = hide;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false}).then(function(stream) {
+    var video = document.getElementById('video');
+    video.src = URL.createObjectURL(stream);
+
+    var horizontal_canvas = document.createElement('canvas');
+    var horizontal_context = horizontal_canvas.getContext('2d');
+    horizontal_canvas.width = window.innerWidth - video.width;
+    horizontal_canvas.height = video.height;
+    horizontal_canvas.style.position = 'absolute';
+    horizontal_canvas.style.visibility = 'hidden';
+    horizontal_canvas.style.left = video.width + 'px';
+    horizontal_canvas.style.top = '0px';
+    document.body.appendChild(horizontal_canvas);
+
+    var vertical_canvas = document.createElement('canvas');
+    var vertical_context = vertical_canvas.getContext('2d');
+    vertical_canvas.width = video.width;
+    vertical_canvas.height = window.innerHeight - video.height;
+    vertical_canvas.style.position = 'absolute';
+    vertical_canvas.style.visibility = 'hidden';
+    vertical_canvas.style.left = '0px';
+    vertical_canvas.style.top = video.height + 'px';
+    document.body.appendChild(vertical_canvas);
+
+    var video_slit_canvas = document.createElement('canvas');
+    var video_slit_context = video_slit_canvas.getContext('2d');
+    video_slit_canvas.width = video.width;
+    video_slit_canvas.height = video.height;
+    video_slit_canvas.style.position = 'absolute';
+    video_slit_canvas.style.left = video.width + 'px';
+    video_slit_canvas.style.top = '0px';
+    document.body.appendChild(video_slit_canvas);
+
+    var video_history = [];
+
+    var options = {
+      x: Math.floor(video.width / 2),
+      y: Math.floor(video.height / 2),
+      strip_mode: false,
+      order: 'top-bottom',
+      x_cycle: {
+        mode: 'disabled',
+        range: 1.0,
+        length: 20
+      },
+      y_cycle: {
+        mode: 'disabled',
+        range: 1.0,
+        length: 20
+      }
+    };
+
+    var horizontal_guide_line = document.createElement('div');
+    horizontal_guide_line.style.visibility = 'hidden';
+    modify_line(horizontal_guide_line, 0, options.y, video.width, options.y);
+    document.body.appendChild(horizontal_guide_line);
+
+    var vertical_guide_line = document.createElement('div');
+    vertical_guide_line.style.visibility = 'hidden';
+    modify_line(vertical_guide_line, options.x, 0, options.x, video.height);
+    document.body.appendChild(vertical_guide_line);
+
+    var gui = new dat.GUI();
+    gui.add(options, 'strip_mode').name("Strip mode").onChange(function(strip_mode) {
+      hide_gui_folder(gui, 'X', !strip_mode);
+      hide_gui_folder(gui, 'Y', !strip_mode);
+      hide_gui_element(gui, 'order', strip_mode);
+
+      horizontal_canvas.style.visibility = strip_mode ? 'visible' : 'hidden';
+      vertical_canvas.style.visibility = strip_mode ? 'visible' : 'hidden';
+      video_slit_canvas.style.visibility = strip_mode ? 'hidden' : 'visible';
+
+      horizontal_guide_line.style.visibility = strip_mode ? 'visible' : 'hidden';
+      vertical_guide_line.style.visibility = strip_mode ? 'visible' : 'hidden';
+    }).listen();
+
+    var x_folder = gui.addFolder("X");
+    x_folder.closed = false;
+    x_folder.add(options, 'x', 0, video.width - 1).name("X").listen();
+
+    x_cycle = x_folder.addFolder("Cycle");
+    x_cycle.add(options.x_cycle, 'mode', ['disabled', 'sin', 'cos', 'triangular']).name("Mode");
+    x_cycle.add(options.x_cycle, 'range', 0, 1).name('Range %');
+    x_cycle.add(options.x_cycle, 'length', 5, 60).name('Length');
+
+    var y_folder = gui.addFolder("Y");
+    y_folder.closed = false;
+    y_folder.add(options, 'y', 0, video.height - 1).name("Y").listen();
+
+    y_cycle = y_folder.addFolder("Cycle");
+    y_cycle.add(options.y_cycle, 'mode', ['disabled', 'sin', 'cos', 'triangular']).name("Mode");
+    y_cycle.add(options.y_cycle, 'range', 0, 1).name('Range %');
+    y_cycle.add(options.y_cycle, 'length', 5, 60).name('Length');
+
+    gui.add(options, 'order', ['top-bottom', 'bottom-top', 'left-right', 'right-left']);
+
+    hide_gui_folder(gui, 'X', true);
+    hide_gui_folder(gui, 'Y', true);
+
+    var mousedown = false;
+
+    video.addEventListener('mousedown', function(event) {
+      if (event.which === 1) {
+        mousedown = true;
+        options.x = event.pageX;
+        options.y = event.pageY;
+      }
+    });
+
+    video.addEventListener('mousemove', function(event) {
+      if (mousedown) {
+        options.x = event.pageX;
+        options.y = event.pageY;
+      }
+    });
+
+    video.addEventListener('mouseup', function(event) {
+      if (event.which === 1) {
+        mousedown = false;
+        options.x = event.pageX;
+        options.y = event.pageY;
+      }
+    });
+
+    var horizontal_strip = document.createElement('canvas');
+    horizontal_strip.width = video.width;
+    horizontal_strip.height = 1;
+    var horizontal_strip_ctx = horizontal_strip.getContext('2d');
+    var horizontal_strip_data = horizontal_strip_ctx.getImageData(0, 0, video.width, 1).data;
+
+    var update_slit_scan = function(timestamp) {
+      if (options.strip_mode) {
+        if (options.x_cycle.mode === 'sin') {
+          options.x = Math.sin(timestamp / 1000 / options.x_cycle.length * Math.PI * 2) * options.x_cycle.range * video.width / 2 + video.width / 2;
+        } else if (options.x_cycle.mode === 'cos') {
+          options.x = Math.cos(timestamp / 1000 / options.x_cycle.length * Math.PI * 2) * options.x_cycle.range * video.width / 2 + video.width / 2;
+        } else if (options.x_cycle.mode === 'triangular') {
+          options.x = 2 * options.x_cycle.range / options.x_cycle.length * (Math.abs((timestamp / 1000) % options.x_cycle.length - options.x_cycle.length / 2) - options.x_cycle.length / 4) * video.width + video.width / 2;
+        }
+
+        if (options.y_cycle.mode === 'sin') {
+          options.y = Math.sin(timestamp / 1000 / options.y_cycle.length * Math.PI * 2) * options.y_cycle.range * video.height / 2 + video.height / 2;
+        } else if (options.y_cycle.mode === 'cos') {
+          options.y = Math.cos(timestamp / 1000 / options.y_cycle.length * Math.PI * 2) * options.y_cycle.range * video.height / 2 + video.height / 2;
+        } else if (options.y_cycle.mode === 'triangular') {
+          options.y = 2 * options.y_cycle.range / options.y_cycle.length * (Math.abs((timestamp / 1000) % options.y_cycle.length - options.y_cycle.length / 2) - options.x_cycle.length / 4) * video.height + video.height / 2;
+        }
+
+        modify_line(horizontal_guide_line, 0, options.y, video.width, options.y);
+        modify_line(vertical_guide_line, options.x, 0, options.x, video.height);
+
+        horizontal_strip_ctx.drawImage(video, options.x * (video.videoWidth / video.width), 0, 1, video.videoHeight, 0, 0, 1, video.height);
+        var new_horizontal_strip_data = horizontal_strip_ctx.getImageData(0, 0, video.width, 1).data;
+
+        if (!buffer_equal(horizontal_strip_data, new_horizontal_strip_data)) {
+          horizontal_strip_data = new_horizontal_strip_data;
+
+          horizontal_context.drawImage(horizontal_canvas, 1, 0);
+          vertical_context.drawImage(vertical_canvas, 0, 1);
+
+          horizontal_context.drawImage(video, options.x * (video.videoWidth / video.width), 0, 1, video.videoHeight, 0, 0, 1, video.height);
+          vertical_context.drawImage(video, 0, options.y * (video.videoHeight / video.height), video.videoWidth, 1, 0, 0, video.width, 1);
+        }
+      } else {
+        var new_frame = document.createElement('canvas');
+        new_frame.width = video.width;
+        new_frame.height = video.height;
+        new_frame.getContext('2d').drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, video.width, video.height);
+
+        video_history.unshift(new_frame);
+        if (video_history.length > Math.max(video.width, video.height)) {
+          video_history.pop();
+        }
+
+        if (options.order === 'top-bottom') {
+          for (var i = 0; i < Math.min(video_history.length, video.height); i++) {
+            video_slit_context.drawImage(video_history[i], 0, i, video.width, 1, 0, i, video.width, 1);
+          }
+        } else if (options.order === 'bottom-top') {
+          for (var i = 0; i < Math.min(video_history.length, video.height); i++) {
+            video_slit_context.drawImage(video_history[i], 0, video.height - i - 1, video.width, 1, 0, video.height - i - 1, video.width, 1);
+          }
+        } else if (options.order === 'left-right') {
+          for (var i = 0; i < Math.min(video_history.length, video.width); i++) {
+            video_slit_context.drawImage(video_history[i], i, 0, 1, video.height, i, 0, 1, video.height);
+          }
+        } else if (options.order === 'right-left') {
+          for (var i = 0; i < Math.min(video_history.length, video.width); i++) {
+            video_slit_context.drawImage(video_history[i], video.width - i - 1, 0, 1, video.height, video.width - i - 1, 0, 1, video.height);
+          }
+        }
+      }
+
+      requestAnimationFrame(update_slit_scan);
+    };
+
+    requestAnimationFrame(update_slit_scan);
+  }).catch(function(error) {
+    console.log(error.toString());
+    console.log(error);
+    alert('You have to enable the webcam for this demo to work');
+  });
+});
---
+++ sort/sort.js
@@ -0,0 +1,547 @@
+'use strict';
+
+var PIXEL_RATIO = 1;
+
+function color(x, width) {
+  return "hsla(" + (x / width * 360) + ", 100%, 50%, 1.0)";
+  //return "RGB(" + (x / width * 255) + ", 0, 0)";
+}
+
+function shuffle(array, start, end) {
+  var m = end - start;
+  while (m) {
+    var i = Math.floor(random() * m--);
+    var tmp = array[start + m];
+    array[start + m] = array[start + i];
+    array[start + i] = tmp;
+  }
+}
+
+var SG_MAGICCONST = 1.0 + Math.log(4.5);
+var LOG4 = Math.log(4.0);
+
+function gamma_distribution(alpha, beta) {
+  /*if (alpha <= 0.0 || beta <= 0.0) {
+    throw new Error('gamma_distribution: alpha and beta must be > 0.0');
+  }*/
+
+  if (alpha > 1) {
+    var ainv = Math.sqrt(2.0 * alpha - 1.0);
+    var bbb = alpha - LOG4;
+    var ccc = alpha + ainv;
+
+    while (true) {
+      var u1 = Math.random();
+      if (1e-7 > u1 || u1 > .9999999) {
+        continue;
+      }
+      var u2 = 1.0 - Math.random();
+      var v = Math.log(u1 / (1.0 - u1)) / ainv;
+      var x = alpha * Math.exp(v);
+      var z = u1 * u1 * u2;
+      var r = bbb + ccc * v - x;
+      if (r + SG_MAGICCONST - 4.5 * z >= 0.0 || r >= Math.log(z)) {
+        return x * beta;
+      }
+    }
+  } else if (alpha === 1.0) {
+      do {
+        var u = Math.random();
+      } while (u <= 1e-7);
+      return -Math.log(u) * beta;
+  } else {
+    while (true) {
+      var u = Math.random();
+      var b = (Math.E + alpha) / Math.E;
+      var p = b * u;
+      if (p <= 1.0) {
+        x = Math.pow(p, (1.0 / alpha));
+      } else {
+        x = -Math.log((b - p) / alpha);
+      }
+      var u1 = Math.random();
+      if (p > 1.0) {
+        if (u1 <= Math.pow(x, alpha - 1.0)) {
+          break;
+        }
+      } else if (u1 <= Math.exp(-x)) {
+        break;
+      }
+    }
+    return x * beta;
+  }
+}
+
+function beta_distribution(alpha, beta) {
+  var y = gamma_distribution(alpha, 1);
+  if (y === 0) {
+    return 0.0;
+  } else {
+    return y / (y + gamma_distribution(beta, 1));
+  }
+}
+
+function random() {
+  do {
+    var ret = beta_distribution(options.distribution.alpha, options.distribution.beta);
+  } while (ret >= 1);
+  return ret;
+}
+
+function SortingVisualization(data, ctx) {
+  this.original_data = [];
+  this.swaps = [];
+  for (var i = 0; i < data.length; i++) {
+    this.original_data.push(data[i].slice());
+    this.swaps.push([]);
+  }
+
+  this.data = data;
+  this.ctx = ctx;
+}
+
+SortingVisualization.prototype.cmp = function(x, y) {
+  return x < y;
+};
+
+SortingVisualization.prototype.swap = function(y, x1, x2) {
+  var temp = this.data[y][x1];
+  this.data[y][x1] = this.data[y][x2];
+  this.data[y][x2] = temp;
+
+  this.swaps[y].push([x1, x2]);
+};
+
+SortingVisualization.prototype.sort_end = function() {
+  for (var y = 0; y < this.swaps.length; y++) {
+    this.swaps[y].reverse();
+  }
+
+  this.data = this.original_data;
+};
+
+SortingVisualization.prototype.step = function() {
+  var swapped = false;
+
+  for (var y = 0; y < this.swaps.length; y++) {
+    if (this.swaps[y].length) {
+      swapped = true;
+
+      var step = this.swaps[y].pop();
+      var x1 = step[0], x2 = step[1];
+
+      var temp = this.original_data[y][x1];
+      this.original_data[y][x1] = this.original_data[y][x2];
+      this.original_data[y][x2] = temp;
+
+      this.ctx.fillStyle = color(this.original_data[y][x1], this.original_data[y].length);
+      this.ctx.fillRect(x1, y, 1, 1);
+
+      this.ctx.fillStyle = color(this.original_data[y][x2], this.original_data[y].length);
+      this.ctx.fillRect(x2, y, 1, 1);
+    }
+  }
+
+  return !swapped;
+};
+
+SortingVisualization.prototype.pivot = function(y, left, right) {
+  if (options.pivot === 'Start') {
+    return left;
+  } else if (options.middle === 'Middle') {
+    return left + Math.floor((right - left) / 2);
+  } else if (options.pivot === 'End') {
+    return right;
+  } else if (options.pivot === 'Random') {
+    return left + Math.floor(random() * (right - left));
+  }
+}
+
+
+function BubbleSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+BubbleSort.prototype = Object.create(SortingVisualization.prototype);
+BubbleSort.prototype.constructor = SortingVisualization;
+
+BubbleSort.prototype.sort = function(y, left, right) {
+  for (; left <= right; left++) {
+    for (var x = left; x <= right; x++) {
+      if (this.cmp(this.data[y][x], this.data[y][left])) {
+        this.swap(y, left, x);
+      }
+    }
+  }
+};
+
+
+function InsertionSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+InsertionSort.prototype = Object.create(SortingVisualization.prototype);
+InsertionSort.prototype.constructor = SortingVisualization;
+
+InsertionSort.prototype.sort = function(y, left, right) {
+  for (; left <= right; left++) {
+    var j = left;
+    while (j > 0 && this.cmp(this.data[y][j], this.data[y][j - 1])) {
+      this.swap(y, j, j - 1);
+      j--;
+    }
+  }
+};
+
+
+function SelectionSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+SelectionSort.prototype = Object.create(SortingVisualization.prototype);
+SelectionSort.prototype.constructor = SortingVisualization;
+
+SelectionSort.prototype.sort = function(y, left, right) {
+  for (; left <= right; left++) {
+    var min_i = left;
+    for (var i = left + 1; i <= right; i++) {
+      if (this.cmp(this.data[y][i], this.data[y][min_i])) {
+        min_i = i;
+      }
+    }
+    this.swap(y, left, min_i);
+  }
+};
+
+
+function CocktailSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+CocktailSort.prototype = Object.create(SortingVisualization.prototype);
+CocktailSort.prototype.constructor = SortingVisualization;
+
+CocktailSort.prototype.sort = function(y, left, right) {
+  var swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (var i = left; i < right - 1; i++) {
+      if (this.cmp(this.data[y][i + 1], this.data[y][i])) {
+        this.swap(y, i, i + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+    swapped = false;
+    for (var i = right - 1; i >= 0; i--) {
+      if (this.cmp(this.data[y][i + 1], this.data[y][i])) {
+        this.swap(y, i, i + 1);
+        swapped = true;
+      }
+    }
+  }
+};
+
+
+function OddEvenSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+OddEvenSort.prototype = Object.create(SortingVisualization.prototype);
+OddEvenSort.prototype.constructor = SortingVisualization;
+
+OddEvenSort.prototype.sort = function(y, left, right) {
+  var sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (var i = left + 1; i < right - 1; i += 2) {
+      if (this.cmp(this.data[y][i + 1], this.data[y][i])) {
+        this.swap(y, i, i + 1);
+        sorted = false;
+      }
+    }
+
+    for (var i = left; i < right - 1; i += 2) {
+      if (this.cmp(this.data[y][i + 1], this.data[y][i])) {
+        this.swap(y, i, i + 1);
+        sorted = false;
+      }
+    }
+  }
+};
+
+
+function ShellSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+ShellSort.prototype = Object.create(SortingVisualization.prototype);
+ShellSort.prototype.constructor = SortingVisualization;
+
+ShellSort.prototype.sort = function(y, left, right) {
+  var gaps = [701, 301, 132, 57, 23, 10, 4, 1];
+
+  for (var k = 0; k < gaps.length; k++) {
+    var gap = gaps[k];
+    for (var i = gap; i <= right; i++) {
+      var temp = this.data[y][i];
+      for (var j = i; j >= gap && this.cmp(temp, this.data[y][j - gap]); j -= gap) {
+        this.swap(y, j, j - gap);
+      }
+      //this.data[y][j] = temp;
+    }
+  }
+};
+
+
+function QuickSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+QuickSort.prototype = Object.create(SortingVisualization.prototype);
+QuickSort.prototype.constructor = SortingVisualization;
+
+QuickSort.prototype.partition = function(y, pivot, left, right) {
+  var store_index = left,
+      pivot_value = this.data[y][pivot];
+
+  this.swap(y, pivot, right);
+
+  for (var v = left; v < right; v++) {
+    if (this.cmp(this.data[y][v], pivot_value)) {
+      this.swap(y, v, store_index++);
+    }
+  }
+
+  this.swap(y, right, store_index);
+
+  return store_index;
+};
+
+QuickSort.prototype.sort = function(y, left, right) {
+  if (left > right) {
+    return;
+  }
+
+  var pivot = this.pivot(y, left, right);
+  var new_pivot = this.partition(y, pivot, left, right);
+
+  this.sort(y, left, new_pivot - 1);
+  this.sort(y, new_pivot + 1, right);
+};
+
+
+function MergeSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+MergeSort.prototype = Object.create(SortingVisualization.prototype);
+MergeSort.prototype.constructor = SortingVisualization;
+
+MergeSort.prototype.merge = function(y, left, right) {
+}
+
+MergeSort.prototype.sort = function(y, left, right) {
+  if (left > right) {
+    return;
+  }
+
+};
+
+
+function HeapSort() {
+  SortingVisualization.apply(this, arguments);
+}
+
+HeapSort.prototype = Object.create(SortingVisualization.prototype);
+HeapSort.prototype.constructor = SortingVisualization;
+
+HeapSort.prototype.max_heapify = function(y, i, length) {
+  while (true) {
+    var left = i * 2 + 1;
+    var right = i * 2 + 2;
+    var largest = i;
+
+    if (left < length && this.cmp(this.data[y][largest], this.data[y][left])) {
+      largest = left;
+    }
+
+    if (right < length && this.cmp(this.data[y][largest], this.data[y][right])) {
+      largest = right;
+    }
+
+    if (i === largest) {
+      break;
+    }
+
+    this.swap(y, i, largest);
+    i = largest;
+  }
+};
+
+HeapSort.prototype.heapify = function(y, length) {
+  for (var i = Math.floor(length / 2); i >= 0; i--) {
+    this.max_heapify(y, i, length);
+  }
+};
+
+HeapSort.prototype.sort = function(y, left, right) {
+  this.heapify(y, right);
+
+  for (var i = right; i > 0; i--) {
+    this.swap(y, i, 0);
+    this.max_heapify(y, 0, i - 1);
+  }
+};
+
+
+var options;
+
+document.addEventListener('DOMContentLoaded', function() {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var processing = false;
+
+  var data, sort_visualization;
+
+  var algorithms = {
+    "Bubble sort": BubbleSort,
+    "Insertion sort": InsertionSort,
+    "Selection sort": SelectionSort,
+    "Cocktail sort": CocktailSort,
+    "Odd-even sort": OddEvenSort,
+    "Shell sort": ShellSort,
+    "Quick sort": QuickSort,
+    "Heap sort": HeapSort
+  }
+
+  options = {
+    width: 300,
+    height: 100,
+    speed: 1,
+    algorithm: 'Bubble sort',
+    pivot: 'Start',
+    generate: 'Increasing',
+    shuffle: function() {
+      for (var y = 0; y < data.length; y++) {
+        shuffle(data[y], 0, data[y].length);
+      }
+      draw();
+    },
+    start: function() {
+      hide_gui_element('shuffle', true);
+      hide_gui_element('start', true);
+      hide_gui_element('stop', false);
+
+      processing = true;
+
+      var algorithm = algorithms[options.algorithm];
+      sort_visualization = new algorithm(data, ctx);
+
+      for (var y = 0; y < options.height; y++) {
+        sort_visualization.sort(y, 0, options.width - 1);
+      }
+      sort_visualization.sort_end();
+
+      function step() {
+        if (!processing) {
+          return;
+        }
+        for (var i = 0; i < options.speed; i++) {
+          if (sort_visualization.step()) {
+            options.stop();
+            return;
+          }
+        }
+        requestAnimationFrame(step);
+      }
+
+      step();
+    },
+    stop: function() {
+      hide_gui_element('shuffle', false);
+      hide_gui_element('start', false);
+      hide_gui_element('stop', true);
+
+      data = sort_visualization.data;
+
+      processing = false;
+    },
+    distribution: {
+      alpha: 1,
+      beta: 1
+    }
+  };
+
+  function draw() {
+    for (var y = 0; y < canvas.height; y++) {
+      for (var x = 0; x < canvas.width; x++) {
+        ctx.fillStyle = color(data[y][x], canvas.width);
+        ctx.fillRect(x, y, 1, 1);
+      }
+    }
+  }
+
+  function resize() {
+    if (processing) {
+      return;
+    }
+
+    canvas.style.width  = options.width  + 'px';
+    canvas.style.height = options.height + 'px';
+
+    canvas.width  = options.width  * PIXEL_RATIO;
+    canvas.height = options.height * PIXEL_RATIO;
+
+    data = [];
+    for (var y = 0; y < canvas.height; y++) {
+      data.push([]);
+      if (options.generate === 'Increasing') {
+        for (var x = 0; x < canvas.width; x++) {
+          data[y].push(x);
+        }
+      } else if (options.generate === 'Decreasing') {
+        for (var x = canvas.width - 1; x >= 0; x--) {
+          data[y].push(x);
+        }
+      }
+    }
+
+    draw();
+  }
+
+  resize();
+
+  var gui = new dat.GUI();
+  gui.add(options, 'width', 1, window.innerWidth * PIXEL_RATIO, 1).name('Width').onChange(resize);
+  gui.add(options, 'height', 1, window.innerHeight * PIXEL_RATIO, 1).name('Height').onChange(resize);
+  gui.add(options, 'speed', 1, 25, 1).name('Speed');
+  gui.add(options, 'algorithm', Object.keys(algorithms)).name('Algorithm').onChange(function() {
+    hide_gui_element('pivot', options.algorithm !== 'Quick sort');
+  });
+  gui.add(options, 'pivot', ['Start', 'Middle', 'End', 'Random']).name('Pivot');
+  gui.add(options, 'generate', ['Increasing', 'Decreasing']).name('Generate').onChange(resize);
+  gui.add(options, 'shuffle').name('Shuffle');
+  gui.add(options, 'start').name('Start');
+  gui.add(options, 'stop').name('Stop');
+
+  var distribution_folder = gui.addFolder('Beta distribution');
+  distribution_folder.add(options.distribution, 'alpha', 0.01).name('Alpha');
+  distribution_folder.add(options.distribution, 'beta', 0.01).name('Beta');
+
+  var hide_gui_element = function(property, hide) {
+    for (var i = 0; i < gui.__controllers.length; i++) {
+      var controller = gui.__controllers[i];
+      if (controller.property === property) {
+        controller.domElement.parentElement.parentElement.hidden = hide;
+        return;
+      }
+    }
+  };
+
+  hide_gui_element('stop', true);
+  hide_gui_element('pivot', true);
+});
