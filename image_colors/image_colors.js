2f21ebc331a8e9a057959d068d9f69c5b7bd700b
Francisco <franciscouzo@gmail.com>
2017-10-10 16:16:59 -0300

    Use plotly instead of vis.js

--- image_colors/image_colors.js
+++ image_colors/image_colors.js
@@ -32,28 +32,15 @@ document.addEventListener('DOMContentLoaded', function() {
     return rgb;
   }
 
-  var graph;
-  var graph_options = {
-    style: 'dot-color',
-    showPerspective: true,
-    showGrid: true,
-    showLegend: false,
-    keepAspectRatio: true,
-    verticalRatio: 1.0,
-    backgroundColor: '#FFFFFF',
-    dotSizeRatio: 0.01,
-    cameraPosition: {
-      distance: 5
-    }
-  };
+  delete color_spaces.lch;
+  delete color_spaces.apple;
+
+  var layout;
 
   var generate = function() {
     if (!image) {
       return;
     }
-
-    graph_options.width = window.innerWidth + 'px';
-    graph_options.height = window.innerHeight + 'px';
 
     var canvas = document.createElement('canvas');
     var ctx = canvas.getContext('2d');
@@ -71,7 +58,10 @@ document.addEventListener('DOMContentLoaded', function() {
     ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
     var image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    var data = new vis.DataSet();
+    var x = [];
+    var y = [];
+    var z = [];
+    var colors = [];
 
     for (var i = 0; i < image_data.width * image_data.height * 4; i += 4) {
       var r = image_data.data[i];
@@ -80,17 +70,79 @@ document.addEventListener('DOMContentLoaded', function() {
 
       var xyz = color_spaces[options.color_space]([r, g, b]);
 
-      var style = 'rgb(' + [r, g, b].join() + ')';
-
-      data.add({x: xyz[0], y: xyz[1], z: xyz[2], style: style});
+      x.push(xyz[0]);
+      y.push(xyz[1]);
+      z.push(xyz[2]);
+      colors.push('rgb(' + [r, g, b].join() + ')');
     }
 
-    if (graph) {
-      graph_options.cameraPosition = graph.getCameraPosition();
+    var trace = {
+      x: x,
+      y: y,
+      z: z,
+      mode: 'markers',
+      type: 'scatter3d',
+      name: 'Colors',
+      marker: {
+        size: options.dot_size,
+        color: colors
+      }
+    };
+
+    var data = [trace];
+
+    layout = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      scene: {
+        camera: {
+          eye: layout ? layout.scene.camera.eye : {x: 0, y: -2, z: 1},
+          center: {x: 0, y: 0, z: 0}
+        }
+      },
+      margin: {
+        l: 0,
+        r: 0,
+        t: 0,
+        b: 0,
+        pad: 0
+      }
+    };
+
+    if (options.color_space === 'rgb') {
+        layout.scene.xaxis = {title: 'R', range: [0, 255]};
+        layout.scene.yaxis = {title: 'G', range: [0, 255]};
+        layout.scene.zaxis = {title: 'B', range: [0, 255]};
+    } else if (options.color_space === 'hsl') {
+        layout.scene.xaxis = {title: 'H', range: [0, 360]};
+        layout.scene.yaxis = {title: 'S', range: [0, 100]};
+        layout.scene.zaxis = {title: 'L', range: [0, 100]};
+    } else if (options.color_space === 'hsv') {
+        layout.scene.xaxis = {title: 'H', range: [0, 360]};
+        layout.scene.yaxis = {title: 'S', range: [0, 100]};
+        layout.scene.zaxis = {title: 'V', range: [0, 100]};
+    } else if (options.color_space === 'hwb') {
+        layout.scene.xaxis = {title: 'H', range: [0, 360]};
+        layout.scene.yaxis = {title: 'W', range: [0, 100]};
+        layout.scene.zaxis = {title: 'B', range: [0, 100]};
+    } else if (options.color_space === 'xyz') {
+        layout.scene.xaxis = {title: 'X', range: [0, 100]};
+        layout.scene.yaxis = {title: 'Y', range: [0, 100]};
+        layout.scene.zaxis = {title: 'Z', range: [0, 100]};
+    } else if (options.color_space === 'lab') {
+        layout.scene.xaxis = {title: 'L*', range: [0, 100]};
+        layout.scene.yaxis = {title: 'a*', range: [-86.185, 98.254]};
+        layout.scene.zaxis = {title: 'b*', range: [-107.863, 94.482]};
+    } else if (options.color_space === 'hcg') {
+        layout.scene.xaxis = {title: 'H', range: [-180, 180]};
+        layout.scene.yaxis = {title: 'C', range: [0, 100]};
+        layout.scene.zaxis = {title: 'G', range: [0, 100]};
     }
 
-    var container = document.getElementById('graph');
-    graph = new vis.Graph3d(container, data, graph_options);
+    Plotly.purge('graph');
+    Plotly.newPlot('graph', data, layout, {
+        displayModeBar: false
+    });
   };
 
   window.addEventListener('resize', generate);
@@ -101,6 +153,7 @@ document.addEventListener('DOMContentLoaded', function() {
       image_upload.click();
     },
     points: 5000,
+    dot_size: 5,
     color_space: 'rgb'
   };
 
@@ -108,6 +161,5 @@ document.addEventListener('DOMContentLoaded', function() {
   gui.add(options, 'load_image').name('Load image');
   gui.add(options, 'points', 1000, 25000, 100).name('Points').onChange(generate);
   gui.add(options, 'color_space', Object.keys(color_spaces)).name('Color space').onChange(generate);
-  gui.add(graph_options, 'dotSizeRatio', 0.001, 0.05, 0.001).name('Dot size ratio').onChange(generate);
-  gui.addColor(graph_options, 'backgroundColor').name('Background color').onChange(generate);
+  gui.add(options, 'dot_size', 1, 20, 1).name('Dot size').onChange(generate);
 });
