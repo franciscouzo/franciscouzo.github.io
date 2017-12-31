1e766ea4f77c39291bd52abff0fe81b0e85ce919
Francisco <franciscouzo@gmail.com>
2017-12-31 17:53:26 -0300

    Change hide_gui_folder definition

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
-var hide_gui_folder = function(gui, folder_name, hide) {
+function hide_gui_folder(gui, folder_name, hide) {
   var folder = gui.__folders[folder_name];
   folder.domElement.parentElement.hidden = hide;
 };
